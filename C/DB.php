eee92aa7e7608ea9555ded658deb0a7cdfad29ec
dexen deVries <dexen.devries@gmail.com>
2020-07-28 05:14:47 +0200

    hm

--- C/DB.php
+++ C/DB.php
@@ -7,6 +7,7 @@ class DB extends PDO
 		$abs_pathname = INSTALL_DIR .'/' .$relative_pathname;
 		parent::__construct(sprintf('sqlite:%s', $abs_pathname), null, null, [
 			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+			PDO::ATTR_EMULATE_PREPARES => false,
 		]);
 		$this->exec('PRAGMA foreign_keys = ON');
 	}
