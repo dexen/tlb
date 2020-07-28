de06a85fb121887140551308a789c8ae49e48489
dexen deVries <dexen.devries@gmail.com>
2020-07-28 05:14:57 +0200

    for whatever reason we're getting CRLF in request
    weeeeird

--- init.php
+++ init.php
@@ -13,3 +13,15 @@ spl_autoload_register(function($className) {
 	require 'C/' .implode('/', $a) .'.php';
 	return true;
 });
+
+(function() {
+	$rr = function($v) {
+		if (is_array($v))
+			return array_map($rr, $v);
+		else if (is_string($v))
+			return str_replace("\r", '', $v);
+		else
+			throw new Exception('unsupported data type');
+	};
+	$_POST = array_map($rr, $_POST);
+})();
