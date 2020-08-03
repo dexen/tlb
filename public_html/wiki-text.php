bb872460a01aba02d05cefe96bf0e84ed1eb6818
dexen deVries <dexen.devries@gmail.com>
2020-08-03 10:10:30 +0200

    cleanup

--- public_html/wiki-text.php
+++ public_html/wiki-text.php
@@ -7,5 +7,4 @@ $slug = $_GET['slug']??null;
 $DB = db_pdo();
 
 $rcd = $DB->queryFetch('SELECT * FROM post_wiki WHERE _url_slug = ?', [ $_GET['slug']??null ]);
-#die(($_GET['slug']??null).'-xatt');
 echo $rcd['body'];
