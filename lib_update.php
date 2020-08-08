ccffb29858fe510851ad28169bd6b06e83abb228
dexen deVries <dexen.devries@gmail.com>
2020-08-08 22:43:09 +0200

    numbered_note

--- lib_update.php
+++ lib_update.php
@@ -15,5 +15,17 @@ function update_the_db()
 	default:
 		throw new Exception(sprintf('unsupported DB version'));
 	case 0:
+		$DB->beginTransaction();
+$DB->exec(<<<EOS
+CREATE TABLE numbered_note (
+	slug TEXT NOT NULL,
+	number INTEGER NOT NULL,
+	body TEXT,
+	PRIMARY KEY (slug, number)
+);
+EOS);
+		update_db_version($DB, 1);
+		$DB->commit();
+	case 1:
 		/* the current version */; }
 }
