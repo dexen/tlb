94649f2086a214da372509535ed5597dd658970a
dexen deVries <dexen.devries@gmail.com>
2020-08-08 02:50:48 +0200

    common style

--- lib_wiki.php
+++ lib_wiki.php
@@ -24,7 +24,7 @@ function wiki_maintenance_refresh_slug_reverse_index_formH() : string
 	$action_name = 'Refresh slug reverse index';
 	return '
 		<form method="post" action="?set=post_wiki&amp;slug=WikiReverseSlugIndex&amp;service=WikiReverseSlugIndex">
-			<button name="action" value="rebuild-slug-reverse-index" style="width: 50%; min-height: 8ex">' .H($action_name) .'</button>
+			<button name="action" value="rebuild-slug-reverse-index" class="strut-12">' .H($action_name) .'</button>
 		</form>';
 }
 
