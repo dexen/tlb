9dc72e6b52e654c474b72cf016f052d5f1422597
dexen deVries <dexen.devries@gmail.com>
2020-08-09 18:57:23 +0200

    split out the custom code

---
+++ public_html/adminer/tlb-adminer-sqlite.php
@@ -0,0 +1,49 @@
+<?php
+
+ini_set('session.save_path', realpath(__DIR__ .'/' .'../../session'));
+
+function td(...$a)
+{
+	foreach ($a as $v)
+		echo htmlspecialchars(var_export($v, true));
+	die('td()');
+}
+
+class SqlitePasswordHandling
+{
+	protected $password_hash;
+
+	function __construct(string $password_hash)
+	{
+		$this->password_hash = $password_hash;
+	}
+
+	function credentials() {
+		$password = get_password();
+		return array(SERVER, $_GET["username"], (password_verify($password, $this->password_hash) ? "" : $password));
+	}
+
+	function login($login, $password) {
+		if ($password != "") {
+			return true;
+		}
+	}
+}
+
+function adminer_object()
+{
+	require 'plugin.php';
+
+	return new class([new SqlitePasswordHandling(require 'sqlite-password-hash.php')]) extends AdminerPlugin {
+
+		function XXXloginFormField($name, $heading, $value) {
+			return parent::loginFormField($name, $heading, str_replace('value="server"', 'value="sqlite"', $value));
+		}
+
+		function XXXdatabase() {
+			return $db_pathname;
+		}
+	};
+}
+
+set_error_handler(null);
