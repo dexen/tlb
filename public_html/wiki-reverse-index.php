2c0e2047630b5b86c3c3518ad725e69fe22fcac5
dexen deVries <dexen.devries@gmail.com>
2020-08-19 14:46:32 +0200

    update for the new slugs

--- public_html/wiki-reverse-index.php
+++ public_html/wiki-reverse-index.php
@@ -7,8 +7,7 @@ $slug = $_GET['slug']??null;
 echo implode(',',
 	array_column(
 		$riA = db_pdo()->queryFetchAll('
-			SELECT p.*
-			FROM post_wiki AS p
-			JOIN _wiki_slug_use AS u ON p.post_id = u.post_id
-			WHERE u._url_slug= ?', [ $slug ]),
-		'_url_slug' ) );
+			SELECT from_slug AS slug
+			FROM _wiki_slug_use AS u
+			WHERE u.to_slug = ?', [ $slug ]),
+		'slug' ) );
